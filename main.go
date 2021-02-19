c94008b1eadfdf713c5a9e8a74db181ef6e0a5a0
nireo <eemil.lehtikangas1@gmail.com>
2021-02-20 01:23:46 +0200

    added a simple client and fixed the server code a bit

--- main.go
+++ main.go
@@ -4,50 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func videoServeHelper(w http.ResponseWriter, r *http.Request, id string) {
-	// format the file name
-	mediaFile := fmt.Sprintf("videos/%s", id)
-
-	// check if the file exists
-	if _, err := os.Stat(mediaFile); !os.IsNotExist(err) {
-		http.Error(w, "video does not exist", http.StatusNotFound)
+func videoServe(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "could not handle post request for this route", http.StatusBadRequest)
 		return
 	}
+
+	id := ps.ByName("id") + ".m3u8"
+
+	mediaFile := fmt.Sprintf("./videos/%s", id)
+	fmt.Println("request")
 
 	// if the file exists, return the file data.
 	http.ServeFile(w, r, mediaFile)
 	w.Header().Set("Content-Type", "application/x-mpegURL")
 }
 
-func videoServe(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "could not handle post request for this route", http.StatusBadRequest)
-		return
-	}
-
-	query := r.URL.Query()
-	ids, ok := query["id"]
-	if !ok || len(ids) == 0 {
-		http.Error(w, "video id was not provided", http.StatusNotFound)
-		return
-	}
-
-	id := ids[0] + ".m3u8"
-	videoServeHelper(w, r, id)
+func serveHlsSegments(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	mediaFile := fmt.Sprintf("./videos/" + ps.ByName("seg"))
+	http.ServeFile(w, r, mediaFile)
+	w.Header().Set("Content-Type", "video/MP2T")
 }
 
-func servePage(w http.ResponseWriter, r *http.Request) {
+func servePage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.ServeFile(w, r, "./static/index.html")
 }
 
 func main() {
-	http.HandleFunc("/stream", videoServe)
-	http.HandleFunc("/", servePage)
+	router := httprouter.New()
+	router.GET("/:id/stream/", videoServe)
+	router.GET("/:id/stream/:seg", serveHlsSegments)
+	router.GET("/", servePage)
 
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+	if err := http.ListenAndServe("localhost:8080", router); err != nil {
 		log.Fatal(err)
 	}
 }
